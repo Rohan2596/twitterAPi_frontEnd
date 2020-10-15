36d203e9bad1f62ec61f80e5dc10e1561d9fd042
Rohan2596 <rohankadam662@gmail.com>
2020-10-15 22:23:54 +0530

    ADDED:Search Component and UPDATED:App.js to project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import SearchComponent from './components/search';
 
 function App() {
   return (
     <div className="App">
-     
+     <span className="main_header">Twitter Search </span>
+     <SearchComponent/>
     </div>
   );
 }
---
+++ src/components/search.jsx
@@ -0,0 +1,14 @@
+import React,{Component } from "react";
+class SearchComponent extends React.Component{
+    render(){
+        return(
+            <>
+
+            <span>
+                Search Bar Component
+            </span>
+            </>
+        )
+    }
+}
+export default SearchComponent;
