4710a3ec82eb220c6cb419ae60cf9922835aaf30
Rohan2596 <rohankadam662@gmail.com>
2020-10-17 14:32:58 +0530

    WIP:Tweets Component to application.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import SearchComponent from './components/search';
+import TweetComponent from './components/tweet';
 
 function App() {
   return (
     <div className="App">
      <span className="main_header">Twitter Search  </span>
      <SearchComponent/>
+     <TweetComponent/>
     </div>
   );
 }
---
+++ src/components/tweet.jsx
@@ -0,0 +1,13 @@
+import React,{Component } from "react";
+import './tweet.css'
+
+class TweetComponent extends Component{
+    render(){
+        return(
+            <>
+            Tweets
+            </>
+        )
+    }
+}
+export default TweetComponent;
