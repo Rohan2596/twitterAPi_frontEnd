3ac35d7e9e493b1dd01b290e05b6790ea6e32b64
Rohan2596 <rohankadam662@gmail.com>
2020-10-21 11:51:19 +0530

    REFRACTED:Code refraction for displaying tweets.

--- src/components/tweet.jsx
+++ src/components/tweet.jsx
@@ -20,26 +20,37 @@ class TweetComponent extends Component{
         });
         
  
-    }   
+    }  
+    displayTweets(){
+        return(
+         <div className="tweetsMain">
+         <div className="avatar">
+             
+         </div>
+             <div className="tweet">
+                 <span className="name">Rohan Kadam</span>
+                 <span className="username">@RohanKadam2596</span>
+                 <span className="text">Checkout my work and give me feedback.</span>
+                 <div className="time">Mon 10.00 am 25/10/2020</div>
+         </div>
+         </div>
+        );
+    } 
      
     componentDidMount(){
         this.getAllTweets()      
        }
+ 
+       
+
+
     render(){
+        
         return(
             <div>
-                <div className="tweetsMain">
-            <div className="avatar">
-                
-            </div>
-                <div className="tweet">
-                    <span className="name">Rohan Kadam</span>
-                    <span className="username">@RohanKadam2596</span>
-                    <span className="text">Checkout my work and give me feedback.</span>
-                    <div className="time">Mon 10.00 am 25/10/2020</div>
-            </div>
-            </div>
-
+        <this.displayTweets/>
+        <this.displayTweets/>
+        <this.displayTweets/>
             </div>
             
         )
