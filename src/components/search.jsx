929650a64e3622538a24ac9c22f05ddc88f7ce9a
Rohan2596 <rohankadam662@gmail.com>
2020-10-24 11:57:18 +0530

    WIP Chip Desgin for username and tag.

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -3,11 +3,46 @@ import './search.css'
 
 
 class SearchComponent extends Component{
+    
+    constructor(props){
+        super(props)
+        this.state={
+            value:'',
+            inputs:[]
+        }
+    }
+    handleChange=(evt)=>{
+        this.setState({
+            value:evt.target.value
+        });
+    }
+    handleKeyDown=(evt)=>{
+        if(['Enter','Tab',','].includes(evt.key)){
+            evt.preventDefault();
+        }
+        var input=this.state.value.trim();
+        if(input){
+            this.setState({
+                inputs:[...this.state.inputs,input],
+                value:''
+            })
+        }
+    }
+
     render(){
         return(
+            <React.Fragment>
+                {this.state.inputs.map(input=>
+                    <div key={input}>{input}</div>)}
             <div className="searchComponent">
-                <input className="searchInput" type="text" name="" placeholder="Search Tweets by @username and #tag"/>
+                <input className="searchInput"
+                 placeholder="Search Tweets by @username and #tag"
+                 value={this.state.value}
+                 onChange={this.handleChange}
+                 onKeyDown={this.handleKeyDown}
+                 />
             </div>
+            </React.Fragment>
         )
     }
 }
