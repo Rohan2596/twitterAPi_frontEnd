38327e68fcc004727cec7e853303ccd01ade85b7
Rohan2596 <rohankadam662@gmail.com>
2020-10-27 10:31:46 +0530

    ADDED:Media image into tweets.

--- src/components/tweet.jsx
+++ src/components/tweet.jsx
@@ -22,6 +22,8 @@ class TweetComponent extends Component{
         new TweetService().getAllTweetsByBoth(inputDto)
         .then(response => {
             this.setState({tweets:response.data.data});
+            console.log(response.data.data);
+            
         }, (err) => {
             console.log("Errorr......"+ JSON.stringify(err.data));
             
@@ -35,18 +37,23 @@ class TweetComponent extends Component{
         this.getAllTweets()      
        }
  
+
+
        
     render(){
      var   displayTweets=this.state.tweets.map((item,i)=>{
             return(
                 <div className="tweetsMain" key={i}>
                 <div className="avatar">
-                    <img src={item.profile_image_url_https} alt="" srcset="" className="avatar_image"/>
+                    <img src={item.profile_image_url_https} alt="" srcSet="" className="avatar_image"/>
                 </div>
                     <div className="tweet">
             <span className="name">{item.profile_name}</span>
             <span className="username">{item.profile_screen_name}</span>
             <span className="text">{item.text}</span>
+     
+          { item.media_image_url_https!==""  && <img src={item.media_image_url_https} alt="" srcSet="" className="tweet_media"/>
+     }
             <div className="time">{item.created_time_stamp}</div>
                 </div>
                 </div>
